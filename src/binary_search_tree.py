b61589f861144f5664f589e9dbcabc3a87f0379a
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-25 16:40:17 +0300

    add search method and tests

--- src/binary_search_tree.py
+++ src/binary_search_tree.py
@@ -1,5 +1,6 @@
 from src.node import Node
 
+
 class BinarySearchTree:
     """Класс для представления бинарного дерева"""
 
@@ -25,6 +26,7 @@ class BinarySearchTree:
                 node.left = Node(data)
             else:
                 self._insert_recursive(node.left, data)
+
         if data['id'] > node.data['id']:
             if node.right is None:
                 node.right = Node(data)
@@ -33,3 +35,18 @@ class BinarySearchTree:
 
     def search(self, post_id: int) -> dict | None:
         """Ищем по id пост и возвращаем словарь с его данными"""
+        if self.__root is None:
+            return None
+        else:
+            return self._search_recursive(self.__root, post_id)
+
+    def _search_recursive(self, node: Node, post_id: int) -> dict | None:
+        """Рекурсивно ищем и возвращаем словарь с данными"""
+        if post_id == node.data['id']:
+            return node.data
+
+        if post_id < node.data['id'] and node.left is not None:
+            return self._search_recursive(node.left, post_id)
+
+        if post_id > node.data['id'] and node.right is not None:
+            return self._search_recursive(node.right, post_id)
